328a6f029831d274248d1f4d4f0e072221d7641c
Jeremygu2000 <jrmy.guyonnet@gmail.com>
2023-12-16 09:18:45 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http')
+
+const server = http.createServer(function(req, res){
+    // Send response
+    console.log('request received on' + req.url)
+
+    if (req.url === '/') {
+        res.end('<h1>Home page</h1>')
+    } else if (req.url === '/about') {
+        res.end('<h1>About page</h1>')
+    } else {
+        res.end('page not found')
+    }    
+})
+
+server.listen(5001, 'localhost', function() {
+    console.log('running')
+})
